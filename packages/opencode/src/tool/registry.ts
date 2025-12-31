fcd78edf201146290a170f8cd5066b006e99055b
heimoshuiyu <heimoshuiyu@gmail.com>
2025-12-31 14:25:23 +0800

    Revert "remove list tool"

--- packages/opencode/src/tool/registry.ts
+++ packages/opencode/src/tool/registry.ts
@@ -5,6 +5,8 @@ import { BashTool } from "./bash"
 import { EditTool } from "./edit"
 import { GlobTool } from "./glob"
 import { GrepTool } from "./grep"
+import { ListTool } from "./ls"
+
 import { ReadTool } from "./read"
 import { TaskTool } from "./task"
 import { TodoWriteTool } from "./todo"
@@ -117,6 +119,7 @@ export const layer: Layer.Layer<
     const edit = yield* EditTool
     const greptool = yield* GrepTool
     const patchtool = yield* ApplyPatchTool
+    const listtool = yield* ListTool
     const skilltool = yield* SkillTool
     const jobkill = yield* JobKillTool
     const joblist = yield* JobListTool
@@ -187,6 +190,7 @@ export const layer: Layer.Layer<
           read: Tool.init(read),
           glob: Tool.init(globtool),
           grep: Tool.init(greptool),
+          list: Tool.init(listtool),
           edit: Tool.init(edit),
           write: Tool.init(writetool),
           task: Tool.init(task),
@@ -213,6 +217,7 @@ export const layer: Layer.Layer<
             tool.read,
             tool.glob,
             tool.grep,
+            tool.list,
             tool.edit,
             tool.write,
             tool.task,
