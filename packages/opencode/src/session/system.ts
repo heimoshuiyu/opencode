78e93b0af705a38fa197849a8e779a26fd3188be
heimoshuiyu <heimoshuiyu@gmail.com>
2026-01-03 02:26:19 +0800

    remove file listing from system prompt

--- packages/opencode/src/session/system.ts
+++ packages/opencode/src/session/system.ts
@@ -36,16 +36,6 @@ export namespace SystemPrompt {
         `  Platform: ${process.platform}`,
         `  Today's date: ${new Date().toDateString()}`,
         `</env>`,
-        `<files>`,
-        `  ${
-          project.vcs === "git" && false
-            ? await Ripgrep.tree({
-                cwd: Instance.directory,
-                limit: 200,
-              })
-            : ""
-        }`,
-        `</files>`,
       ].join("\n"),
     ]
   }
